43d3e1df6e9caf7dbc6c01a284ed4f764b6a34ef
ryckhardpapi <Ryckhard08@gmail.com>
2024-07-24 21:29:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = [":Moneybag: Chasing the bag" ,"Watching Essential Service"];
 
 
 let currentIndex = 0;
